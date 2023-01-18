7a624aaa839800824634c6483934aab1f4f4df88
Sincerely Brittany <sincerelybr@gmail.com>
2023-01-18 13:48:08 -0500

    adds notes about dispatch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrement = () => {};
   return (
     <div className="App">
       <h1> Counter App </h1>
       <h2> {counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
+
+  //dispatch is used whenever you want to send information/data to the redux store
+  //example: updating the state every time the button is clicked
+  //useDispatch help us to dispatch an action, then we have to procide a unique action,
+  //then the redux store will fetch the action and update the store
 }
 
 export default App;
